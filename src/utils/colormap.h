1c39dde888c906f8f0d79c6c95b26d70a9f7caf3
Georges Nader <georgesnader90@gmail.com>
2018-02-13 21:43:09 +0800

    added colormap class

---
+++ src/utils/colormap.h
@@ -0,0 +1,35 @@
+#ifndef COLORMAP_H
+#define COLORMAP_H
+
+#include <Eigen/Core>
+using namespace Eigen;
+
+class ColorMap
+{
+public:
+  enum Type { GREY, SEASHORE, BLACKBODY };
+
+public:
+  ColorMap(Type t = GREY)
+  {
+    setColorMap(t);
+  }
+
+  ~ColorMap(){}
+
+  void setColorMap(Type t);
+
+  void compute_colors(const VectorXd& data, MatrixXd& colors, double min = 0., double max = 1.) const;
+
+protected:
+  inline int getMapSize() const { return m_map.rows(); }
+
+  Eigen::Vector3d get_pixel_value(double value) const;
+
+protected:
+  Eigen::MatrixX3d m_map;
+
+};
+
+#endif // COLORMAP_H
+
